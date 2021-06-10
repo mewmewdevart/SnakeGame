711953ad6d95a17a57739c4be431efc4902611b5
MewMew <mewmewdevart@gmail.com>
2021-06-10 16:00:45 -0300

    joguinho

--- assets/js/game.js
+++ assets/js/game.js
@@ -55,6 +55,15 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over! :(');
+        }
+    }
+
+
     criarBG();
     criarCobrinha();
     drawFood();
