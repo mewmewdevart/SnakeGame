c065ec37a7d17d20436dd369504e85033cd82271
MewMew <mewmewdevart@gmail.com>
2021-06-10 14:41:28 -0300

    criacao do background

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,3 +4,14 @@
                     ║║║║║╩╣║║║║║║╩╣║║║
 Developed By:       ╚╩═╩╩═╩══╩╩╩╩═╩══╝
 */
+
+let canvas = document.getElementById("snake"); /* Selecionando o ID */
+let context = canvas.getContext("2d"); /* Renderizando o desenho - plano 2D */
+let box = 32; /* 32 Pixels cada quadrado do Canvas */
+
+function criarBG()  { /* Desenhando o Canvas */
+    context.fillStyle   =   "#AC5DD9"; /* Color BG */
+    context.fillRect(0, 0, 16 * box, 16 *box); /* Desenho do retangulo : 4 Parametros, posição de x e y altura e largura */
+}
+
+criarBG();
