a6912d1d1f8bce5a920ddaff7627d3074fcf6af8
MewMew <mewmewdevart@gmail.com>
2021-06-10 14:58:39 -0300

    movimento da cobrinha

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,18 +13,40 @@ snake[0] = {
     x:  8 * box,
     y:  8 * box
 }
+let direction = "right";
 
-function criarBG()  { /* Desenhando o Canvas */
+function criarBG(){ /* Desenhando o Canvas */
     context.fillStyle   =   "#AC5DD9"; /* Color BG */
     context.fillRect(0, 0, 16 * box, 16 *box); /* Desenho do retangulo : 4 Parametros, posição de x e y altura e largura */
 }
 
-function criarCobrinha()    {
+function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle   = "#DDA5E9";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box; /*plano cartesiano*/
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeX -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); /* Intervalo para iniciar o jogo */
